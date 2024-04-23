46a6a05d0a598c9863262c1ef62702bb1fa9a89e
can <canevcimen09@gmail.com>
2024-04-23 05:22:45 +0300

    New questions have been added, the scoreKeeper counter has been fixed, and some bugs have been addressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +29,53 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+    setState(() {
+      if(quizBrain.isFinished()==true)
+        {
+
+              scoreKeeper.clear();
+
+        }
+      else
+      {
+
+      }
+
+      if (correctAnswer == userPickedAnswer) {
+        scoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      } else {
+        scoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      }
+
+      quizBrain.NextQuestion();
+    });
+  }
+
+  /* List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  List<bool> answers = [
+    false,
+    true,
+    true,
+  ];
+  */
+
+  // Question q1 = Question('You can lead a cow down stairs but not up stairs.' , false);
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +88,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -51,7 +102,6 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: TextButton(
-
               child: Text(
                 'True',
                 style: TextStyle(
@@ -60,9 +110,10 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                checkAnswer(true);
               },
-              style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.green)),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.green)),
             ),
           ),
         ),
@@ -70,7 +121,6 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: TextButton(
-
               child: Text(
                 'False',
                 style: TextStyle(
@@ -79,13 +129,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                checkAnswer(false);
               },
-              style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.red)),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.red)),
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
